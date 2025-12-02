7d5fbdfcac31bca862058e5fd246b496d62d3e71
kuanter <artemkrezub790@gmail.com>
2025-12-02 01:26:56 +0100

    Copy file from main for test

---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/GridPosition.java
@@ -0,0 +1,12 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+public class GridPosition {
+    public int x;
+    public int y;
+
+    public GridPosition(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
---
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/Points.java
@@ -0,0 +1,13 @@
+package sk.uniba.fmph.dcs.terra_futura;
+
+public final class Points {
+    private final int value;
+
+    public Points(final int value) {
+        this.value = value;
+    }
+
+    public int value() {
+        return value;
+    }
+}
