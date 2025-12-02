439ca0777fa20ff4882acb652b00e96b099ccd43
kuanter <artemkrezub790@gmail.com>
2025-12-02 20:34:21 +0100

    Update GridPosition

--- src/main/java/sk/uniba/fmph/dcs/terra_futura/GridPosition.java
+++ src/main/java/sk/uniba/fmph/dcs/terra_futura/GridPosition.java
@@ -1,12 +1,21 @@
 package sk.uniba.fmph.dcs.terra_futura;
 
-public class GridPosition {
-    public int x;
-    public int y;
+public final class GridPosition {
+    private final int x;
+    private final int y;
 
-    public GridPosition(int x, int y) {
+    public GridPosition(final int x, final int y) {
         this.x = x;
         this.y = y;
     }
-}
 
+    public int x() {
+        return x;
+    }
+
+    public int y() {
+        return y;
+    }
+
+
+}
